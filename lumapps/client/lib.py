b27aea908f25dd12d999b7ad5b0744fe3e59a7fa
Aurelien <aurelien@lumapps.com>
2019-01-20 20:59:52 +0100

    clearify repo

--- lumapps/client/lib.py
+++ lumapps/client/lib.py
@@ -1,6 +1,5 @@
 from __future__ import print_function, unicode_literals
 import json
-import os
 from time import time
 from datetime import datetime, timedelta
 from textwrap import TextWrapper
@@ -10,125 +9,13 @@ from google.auth.transport.requests import AuthorizedSession
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 
-GOOGLE_APIS = ("drive", "admin", "groupssettings")
-FILTERS = {
-    # content/get, content/list, ...
-    "content/*": [
-        "lastRevision",
-        "authorDetails",
-        "updatedByDetails",
-        "writerDetails",
-        "headerDetails",
-        "customContentTypeDetails",
-    ],
-    # community/get, community/list, ...
-    "community/*": [
-        "lastRevision",
-        "authorDetails",
-        "updatedByDetails",
-        "writerDetails",
-        "headerDetails",
-        "customContentTypeDetails",
-        "adminsDetails",
-        "usersDetails",
-    ],
-    "communitytemplate/*": [
-        "lastRevision",
-        "authorDetails",
-        "updatedByDetails",
-        "writerDetails",
-        "headerDetails",
-        "customContentTypeDetails",
-        "adminsDetails",
-        "usersDetails",
-    ],
-    # template/get, template/list, ...
-    "template/*": ["properties/duplicateContent"],
-    "community/post/*": [
-        "authorDetails",
-        "updatedByDetails",
-        "mentionsDetails",
-        "parentContentDetails",
-    ],
-    "comment/get": ["authorProperties", "mentionsDetails"],
-    "comment/list": ["authorProperties", "mentionsDetails"],
-}
-
-
-def pop_matches(dpath, d):
-    if not dpath:
-        return
-    for pth_part in dpath.split("/")[:-1]:
-        if not isinstance(d, dict):
-            return
-        d = d.get(pth_part)
-    if not isinstance(d, dict):
-        return
-    d.pop(dpath.rpartition("/")[2], None)
-
-
-def get_conf_file():
-    if "APPDATA" in os.environ:
-        d = os.environ["APPDATA"]
-    elif "XDG_CONFIG_HOME" in os.environ:
-        d = os.environ["XDG_CONFIG_HOME"]
-    else:
-        d = os.path.join(os.path.expanduser("~"), ".config")
-    if __package__:
-        return os.path.join(d, __package__ + ".conf")
-    else:
-        return os.path.join(d, "lumapps_api_client.conf")
-
-
-def get_conf():
-    try:
-        with open(get_conf_file()) as fh:
-            conf = json.load(fh)
-    except IOError:
-        return {"configs": {}, "cache": {}}
-    if not conf:
-        conf = {"configs": {}, "cache": {}}
-    return conf
-
-
-def set_conf(conf):
-    try:
-        with open(get_conf_file(), "wt") as fh:
-            return json.dump(conf, fh, indent=4)
-    except IOError:
-        pass
-
-
-class ApiCallError(Exception):
-    pass
-
-
-class DiscoveryCache(object):
-    _max_age = 60 * 60 * 24  # 1 day
-
-    @staticmethod
-    def get(url):
-        cached = get_conf()["cache"].get(url)
-        if not cached:
-            return None
-        expiry_dt = datetime.strptime(cached["expiry"][:19], "%Y-%m-%dT%H:%M:%S")
-        if expiry_dt < datetime.now():
-            return None
-        return cached["content"]
-
-    @staticmethod
-    def set(url, content):
-        conf = get_conf()
-        conf["cache"][url] = {
-            "expiry": (
-                datetime.now() + timedelta(seconds=DiscoveryCache._max_age)
-            ).isoformat()[:19],
-            "content": content,
-        }
-        set_conf(conf)
-
-
-cache = DiscoveryCache()
+from lumapps.client.utils import (
+    DiscoveryCache,
+    pop_matches,
+    ApiCallError,
+    GOOGLE_APIS,
+    FILTERS,
+)
 
 
 class ApiClient(object):
@@ -278,7 +165,7 @@ class ApiClient(object):
                 discoveryServiceUrl=self._url,
                 credentials=self.creds,
                 cache_discovery=True,
-                cache=cache,
+                cache=DiscoveryCache(),
             )
         return self._service
 
