8db175d8f178885329dd988a9f433ca82b5b2c64
Aurelien <aurelien@lumapps.com>
2019-01-20 18:31:26 +0100

    Add functionning example

---
+++ examples/UploadMedia.py
@@ -0,0 +1,10 @@
+from lumapps.client.lib import ApiClient
+from lumapps.helpers.media import upload_and_save
+
+TOKEN = "<your_token>"
+client = ApiClient(token=TOKEN)
+
+instance_id = "<your_instance_id>"
+files = ["<filename 1>", "<filename 2>"]
+
+upload_and_save(client, instance=instance_id, files=files)
