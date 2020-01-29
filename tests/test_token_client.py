786750ce5ace57b02842b952edc80998144a3ebe
Ludovic VAUGEOIS PEPIN <ludovicvp@gmail.com>
2020-01-29 15:46:39 +0100

    Add test

---
+++ tests/test_token_client.py
@@ -0,0 +1,25 @@
+from lumapps.api import TokenClient
+
+
+def test_token_client_1(mocker):
+    mocker.patch(
+        "lumapps.api.client.ApiClient.get_call",
+        return_value={"accessToken": "foo1", "expiresAt": "12345"},
+    )
+    cli = TokenClient("11111", token="FOO")
+    assert cli.customer_id == "11111"
+    token, exp = cli.get_token_and_expiry("foo@bar.com")
+    assert token == "foo1"
+    assert exp == 12345
+
+
+def test_token_client_2(mocker):
+    mocker.patch(
+        "lumapps.api.client.ApiClient.get_call",
+        return_value={"accessToken": "foo1", "expiresAt": "12345"},
+    )
+    cli = TokenClient("11111", token="FOO")
+    getter = cli.get_token_getter("foo@bar.com")
+    token, exp = getter()
+    assert token == "foo1"
+    assert exp == 12345
