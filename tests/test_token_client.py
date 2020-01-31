6e2aa069c8a4f57c65a5a8556babfc0a1ffca735
Ludovic VAUGEOIS PEPIN <ludovicvp@gmail.com>
2020-01-31 10:08:55 +0100

    Fix tests

--- tests/test_token_client.py
+++ tests/test_token_client.py
@@ -1,27 +1,29 @@
-import lumapps.api.utils
-import lumapps.api.client
-import lumapps.api.token_client
+from unittest.mock import patch
+
+from lumapps.api.token_client import TokenClient
 
 
-def test_token_client_1(mocker):
-    mocker.patch(
-        "lumapps.api.client.ApiClient.get_call",
+def test_token_client_1():
+    with patch.object(
+        TokenClient,
+        'get_call',
         return_value={"accessToken": "foo1", "expiresAt": "12345"},
-    )
-    cli = lumapps.api.token_client.TokenClient("11111", token="FOO")
-    assert cli.customer_id == "11111"
-    token, exp = cli.get_token_and_expiry("foo@bar.com")
-    assert token == "foo1"
-    assert exp == 12345
+    ) as _:
+        cli = TokenClient("11111", token="FOO")
+        assert cli.customer_id == "11111"
+        token, exp = cli.get_token_and_expiry("foo@bar.com")
+        assert token == "foo1"
+        assert exp == 12345
 
 
-def test_token_client_2(mocker):
-    mocker.patch(
-        "lumapps.api.client.ApiClient.get_call",
+def test_token_client_2():
+    with patch.object(
+        TokenClient,
+        'get_call',
         return_value={"accessToken": "foo1", "expiresAt": "12345"},
-    )
-    cli = lumapps.api.token_client.TokenClient("11111", token="FOO")
-    getter = cli.get_token_getter("foo@bar.com")
-    token, exp = getter()
-    assert token == "foo1"
-    assert exp == 12345
+    ) as _:
+        cli = TokenClient("11111", token="FOO")
+        getter = cli.get_token_getter("foo@bar.com")
+        token, exp = getter()
+        assert token == "foo1"
+        assert exp == 12345
