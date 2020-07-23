28b33a0ad141ff7505ebd4288dacdc21b5c843c0
Ludovic VAUGEOIS PEPIN <ludovicvp@gmail.com>
2020-07-23 13:53:56 +0200

    Fix tests

--- tests/test_token_client.py
+++ tests/test_token_client.py
@@ -1,4 +1,4 @@
-from lumapps.api import TokenClient
+from lumapps.api import LumAppsClient
 
 
 def test_token_client_1(mocker):
@@ -6,7 +6,7 @@ def test_token_client_1(mocker):
         "lumapps.api.client.BaseClient.get_call",
         return_value={"accessToken": "foo1", "expiresAt": "12345"},
     )
-    cli = TokenClient("11111", token="FOO")
+    cli = LumAppsClient("11111", "22222", token="FOO")
     assert cli.customer_id == "11111"
     token, exp = cli._get_token_and_expiry("foo@bar.com")
     assert token == "foo1"
@@ -18,7 +18,7 @@ def test_token_client_2(mocker):
         "lumapps.api.client.BaseClient.get_call",
         return_value={"accessToken": "foo1", "expiresAt": "12345"},
     )
-    cli = TokenClient("11111", token="FOO")
+    cli = LumAppsClient("11111", "22222", token="FOO")
     getter = cli.get_token_getter("foo@bar.com")
     token, exp = getter()
     assert token == "foo1"
